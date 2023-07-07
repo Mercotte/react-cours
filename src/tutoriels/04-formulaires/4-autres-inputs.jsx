b8aebaec45f97c0d28729f5d277b5febe065997c
Mercotte <sbhpro@outlook.fr>
2023-07-07 14:47:00 +0200

    modif cours 4-autres-inputs

--- src/tutoriels/04-formulaires/4-autres-inputs.jsx
+++ src/tutoriels/04-formulaires/4-autres-inputs.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
+
 const frameworks = ['react', 'angular', 'vue', 'svelte'];
 
 const OtherInputs = () => {
+  const [shipping, setShipping] = useState(false);
+  const [framework, setFramework] = useState('react');
+
+  const handleShipping = (e) => {
+    console.log(e.target.checked);
+    setShipping(e.target.checked);
+  };
+
+  const handleFramework = (e) => {
+    console.log(e.target.value);
+    setFramework(e.target.value);
+  };
+
   return (
     <form className='form'>
       <h4>Autres inputs</h4>
@@ -8,6 +23,13 @@ const OtherInputs = () => {
       {/* shipping */}
       <div className='form-row' style={{ textAlign: 'left' }}>
         <input type='checkbox' name='shipping' id='shipping' />
+        <input
+          type='checkbox'
+          name='shipping'
+          id='shipping'
+          checked={shipping}
+          onChange={handleShipping}
+        />
         <label htmlFor='shipping'> Frais de port gratuits</label>
       </div>
 
@@ -17,6 +39,13 @@ const OtherInputs = () => {
           Framework
         </label>
         <select name='framework' id='framework' className='form-input'>
+          <select
+            name='framework'
+            id='framework'
+            className='form-input'
+            value={framework}
+            onChange={handleFramework}
+          />
           {frameworks.map((framework, index) => (
             <option key={index} value={framework}>
               {framework}
@@ -24,12 +53,10 @@ const OtherInputs = () => {
           ))}
         </select>
       </div>
-
       <button type='submit' className='btn btn-block'>
         Envoyer
       </button>
     </form>
   );
 };
-
 export default OtherInputs;

ee627c1d7a62f7e7b47600f9f492cfaf8615f9fe
Mercotte <sbhpro@outlook.fr>
2023-07-07 14:38:09 +0200

    react-cours

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Starter from './tutoriel/04-formulaires/4-autres-inputs.jsx';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Starter />
+    </div>
+  );
+};
+
+export default App;
